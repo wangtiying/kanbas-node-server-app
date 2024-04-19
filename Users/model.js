49bb09fec76b9c6eac6cf7578cc91a2ae16e5ab2
wangtiying <wang.tiy@northeastern.edu>
2024-04-18 23:47:57 -0400

    a6 Mongo sp24

--- Users/model.js
+++ Users/model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import schema from "../Users/schema.js";
+import schema from "./schema.js";
 const model = mongoose.model("UserModel", schema);
 export default model;
 
